c20baee0a9f3eac95ff8ad32d5889265b2c24a92
awnlzw <awnlzw@126.com>
2023-12-31 08:57:32 +0800

    feat: nix flake dev env

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "My Personal NixOS Configuration";
+
+  nixConfig = {};
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      imports = [
+      ];
+      perSystem = {
+        config,
+        inputs',
+        pkgs,
+        system,
+        lib,
+        ...
+      }: {
+        devShells = {
+          #run by `nix devlop` or `nix-shell`(legacy)
+          #Temporarily enable experimental features, run by`nix develop --extra-experimental-features nix-command --extra-experimental-features flakes`
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              git
+              jdk8
+              jdk21
+              maven
+            ];
+            inputsFrom = [
+            ];
+          };
+        };
+      };
+    };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-compat = {
+      url = "github:inclyc/flake-compat";
+      flake = false;
+    };
+  };
+}
